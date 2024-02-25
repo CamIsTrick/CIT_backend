1638a4b0b7b440d16902a20d60ac5c4b5fefac1f
howudong <tjdvy963@naver.com>
2024-02-25 21:54:46 +0900

    feat : create project

---
+++ src/main/java/cit/camistrick/CamistrickApplication.java
@@ -0,0 +1,13 @@
+package cit.camistrick;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CamistrickApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CamistrickApplication.class, args);
+	}
+
+}
