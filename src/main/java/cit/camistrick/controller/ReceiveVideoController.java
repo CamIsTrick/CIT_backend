15fb016450f5660ca1c314101193e88e50794619
TrixieL20 <minjung0768@gmail.com>
2024-03-02 20:45:22 +0900

    feat : sdpAnswer 구현

--- src/main/java/cit/camistrick/controller/ReceiveVideoController.java
+++ src/main/java/cit/camistrick/controller/ReceiveVideoController.java
@@ -18,4 +18,16 @@ public class ReceiveVideoController {
             sender.getSession().sendMessage(new TextMessage(message.toString()));
         }
     }
+
+    public void sdpAnswer(UserSession sender, UserSession receiver, String sdpOffer) throws IOException {
+        log.info("USER {}: SdpOffer for {} receiveVideoFrom", receiver.getName(), sender.getName());
+
+        final JsonObject receiveVideoResponse = new JsonObject();
+        receiveVideoResponse.addProperty("id", "receiveVideoAnswer");
+        receiveVideoResponse.addProperty("name", sender.getName());
+        receiveVideoResponse.addProperty("sdpAnswer", sdpOffer);
+
+        log.info("USER {}: SdpAnswer for {} receiveVideoAnswer", receiver.getName(), sender.getName());
+        this.sendMessage(sender, receiveVideoResponse);
+    }
 }
