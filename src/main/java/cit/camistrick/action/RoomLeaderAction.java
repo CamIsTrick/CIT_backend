9d0cc32b67ddf354ffdfce3401dd8028e5c51a31
TrixieL20 <102663507+TrixieL20@users.noreply.github.com>
2024-03-10 14:34:30 +0900

    Create RoomLeaderAction.java

---
+++ src/main/java/cit/camistrick/action/RoomLeaderAction.java
@@ -0,0 +1,46 @@
+package cit.camistrick.action;
+
+import cit.camistrick.domain.Room;
+import cit.camistrick.service.RoomManager;
+import com.google.gson.JsonObject;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.socket.WebSocketSession;
+
+import java.io.IOException;
+
+@Slf4j
+@RequiredArgsConstructor
+public class RoomLeaderAction implements KurentoAction {
+    private final RoomManager roomManager;
+
+    @Override
+    public void process(WebSocketSession session, JsonObject jsonMessage) throws IOException {
+
+        String userId = jsonMessage.get("id").getAsString();
+        String username = jsonMessage.get("name").getAsString();
+
+        if (userId == null) {
+            log.error("User ID value is null");
+        } else {
+            log.info("id : {}", userId);
+        }
+
+        if (username == null) {
+            log.error("Name value is null");
+        } else {
+            log.info("name : {}", username);
+        }
+        initUserSession(session, username);
+    }
+
+    private void initUserSession(WebSocketSession session, String username) {
+        Room room = roomManager.createRoom();
+        room.join(username, session);
+    }
+
+    @Override
+    public void onError() {
+        log.error("RoomLeaderAction : Error Occurred");
+    }
+}
