353edece65e41c5511e9fbd25da659bdf1d0068d
howudong <tjdvy963@naver.com>
2024-03-14 00:16:24 +0900

    refactor : KurentoActionResolver findAction 내부 구현 수정

--- src/main/java/cit/camistrick/handler/KurentoActionResolver.java
+++ src/main/java/cit/camistrick/handler/KurentoActionResolver.java
@@ -1,9 +1,13 @@
 package cit.camistrick.handler;
 
 import cit.camistrick.action.KurentoAction;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 import lombok.RequiredArgsConstructor;
+import org.jetbrains.annotations.NotNull;
 
 import java.util.Map;
+import java.util.Optional;
 
 @RequiredArgsConstructor
 public class KurentoActionResolver {
@@ -11,11 +15,24 @@ public class KurentoActionResolver {
     private static final String CLOSE_ID = "exit";
     private final Map<String, KurentoAction> handlerMap;
 
-    public KurentoAction findAction(String id) {
-        return handlerMap.getOrDefault(id, handlerMap.get(NOT_MATCH_ID));
+    public KurentoAction findAction(JsonObject jsonMessage) {
+        String id = findKurentoActionById(jsonMessage);
+
+        if (handlerMap.get(id) == null) {
+            return handlerMap.get(NOT_MATCH_ID);
+        }
+        return handlerMap.get(id);
+    }
+
+    @NotNull
+    private String findKurentoActionById(JsonObject jsonMessage) {
+        return Optional
+                .ofNullable(jsonMessage.get("id"))
+                .map(JsonElement::getAsString).
+                orElse("error");
     }
 
     public KurentoAction getCloseAction() {
-        return findAction(CLOSE_ID);
+        return handlerMap.get(CLOSE_ID);
     }
 }
